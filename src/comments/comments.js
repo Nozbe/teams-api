db8c52b93f34afc783bc4c78504db5fa8c431439
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2020-12-23 13:34:10 +0100

    Remove try..catches

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -1,40 +1,31 @@
 const randomId = require("../utils/randomId");
 
 const getComments = async (apiClient, { taskId }) => {
-  try {
-    const { data } = await apiClient.get("sync", {
-      params: {
-        collection_name: "comments",
-      },
-    });
+  const { data } = await apiClient.get("sync", {
+    params: {
+      collection_name: "comments",
+    },
+  });
 
-    const comments = data.changes.comments.updated;
+  const comments = data.changes.comments.updated;
 
-    return comments.filter((comment) => comment.task_id === taskId);
-  } catch (err) {
-    console.error(err);
-  }
+  return comments.filter((comment) => comment.task_id === taskId);
 };
 
-const addComment = async (apiClient, { taskId, commentText }) => {
-  try {
-    await apiClient.post("sync", {
-      comments: {
-        created: [
-          {
-            id: randomId(),
-            task_id: taskId,
-            body: commentText,
-          },
-        ],
-        updated: [],
-        deleted: [],
-      },
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
+const addComment = async (apiClient, { taskId, commentText }) =>
+  await apiClient.post("sync", {
+    comments: {
+      created: [
+        {
+          id: randomId(),
+          task_id: taskId,
+          body: commentText,
+        },
+      ],
+      updated: [],
+      deleted: [],
+    },
+  });
 
 exports = module.exports = {
   getComments,
